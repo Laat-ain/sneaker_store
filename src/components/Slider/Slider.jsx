afd4ab692fb21b975c1dc2d9b0714bbdfd3977b2
Natalia <ohrim2228n@mail.ru>
2024-03-22 00:56:20 +0300

    corrected slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,95 +1,103 @@
-import React, { useRef, useState } from 'react';
-import styles from './Slider.module.css';
-
-import image_1 from '../../public/sneaker_1.jpg';
-import image_3 from '../../public/sneaker_3.jpg';
-import image_4 from '../../public/sneaker_4.jpg';
-import image_5 from '../../public/sneaker_5.png';
-import image_7 from '../../public/sneaker_7.jpg';
-import image_8 from '../../public/sneaker_8.jpg';
-
-
+import React, { useRef, useState } from "react";
+import styles from "./Slider.module.css";
+import Image from "next/image";
 
 const slidesData = [
-    {
-      image: image_3,
-      name: "Lorem 1",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!"
-    },
-    {
-      image: image_7,
-      name: "Lorem 2",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!"
-    },
-    {
-      image: image_8,
-      name: "Lorem 3",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!"
-    },
-    {
-      image: image_5,
-      name: "Lorem 4",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!"
-    },
-    {
-      image: image_4,
-      name: "Lorem 5",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!"
-    },
-    {
-      image: image_1,
-      name: "Lorem 6",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!"
-    },
-  ];
+  {
+    imagePath: "/slider/sneaker_5.png",
+    name: "Lorem 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!",
+  },
+  {
+    imagePath: "/slider/sneaker_7.jpg",
+    name: "Lorem 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!",
+  },
+  {
+    imagePath: "/slider/sneaker_8.jpg",
+    name: "Lorem 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!",
+  },
+  {
+    imagePath: "/slider/sneaker_3.jpg",
+    name: "Lorem 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!",
+  },
+  {
+    imagePath: "/slider/sneaker_4.jpg",
+    name: "Lorem 5",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!",
+  },
+  {
+    imagePath: "/slider/sneaker_2.jpg",
+    name: "Lorem 6",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolore tenetur exercitationem modi sapiente. Illo vel adipisci in ipsa sapiente! Nesciunt accusamus, atque deserunt est dicta repellendus fugiat obcaecati ad!",
+  },
+];
 
-  const Slider = () => {
-    const slideRef = useRef(null);
-    const [currentIndex, setCurrentIndex] = useState(0);
-  
-    const handleNext = () => {
-      if (slideRef.current) {
-        slideRef.current.appendChild(slideRef.current.firstElementChild);
-      }
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % slidesData.length);
-    };
-  
-    const handlePrev = () => {
-      if (slideRef.current) {
-        slideRef.current.prepend(slideRef.current.lastElementChild);
-      }
-      setCurrentIndex((prevIndex) => (prevIndex - 1 + slidesData.length) % slidesData.length);
-    };
-  
-    return (
-      <div className={styles.container}>
-        <div ref={slideRef} className={styles.slide}>
-          {slidesData.map((slide, index) => (
-            <div
-              key={index}
-              className={`${styles.item} ${index === currentIndex ? styles.active : ''}`}
-              //style={{ backgroundImage: `url(${slide.image})` }}
-              //style={{ backgroundImage: `url(${br.src})` }}
-              style={{ backgroundImage: `url(${slide.image.src})` }}  
-            >
-              <div className={styles.content}>
-                <div className={styles.name}>{slide.name}</div>
-                <div className={styles.des}>{slide.description}</div>
-              </div>
-            </div>
-          ))}
-        </div>
-  
-        <div className={styles.button}>
-          <button className={styles.prev} onClick={handlePrev}>
-            Назад
-          </button>
-          <button className={styles.next} onClick={handleNext}>
-            Вперёд
-          </button>
-        </div>
-      </div>
+const Slider = () => {
+  const slideRef = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    if (slideRef.current) {
+      slideRef.current.appendChild(slideRef.current.firstElementChild);
+    }
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slidesData.length);
+  };
+
+  const handlePrev = () => {
+    if (slideRef.current) {
+      slideRef.current.prepend(slideRef.current.lastElementChild);
+    }
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + slidesData.length) % slidesData.length
     );
   };
-  
-  export default Slider;
+
+  return (
+    <div className={styles.container}>
+      <div ref={slideRef} className={styles.slide}>
+        {slidesData.map((slide, index) => (
+          <div
+            key={index}
+            className={`${styles.item} ${
+              index === currentIndex ? styles.active : ""
+            }`}
+          >
+            <Image
+              src={slide.imagePath}
+              alt={slide.name}
+              fill={true}
+              //className={styles.img}
+              className={slide.imagePath === "/slider/sneaker_2.jpg"?styles.specialPositionImg:styles.positionImg}
+
+              
+            />
+            <div className={styles.content}>
+              <div className={styles.name}>{slide.name}</div>
+              <div className={styles.des}>{slide.description}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.button}>
+        <button className={styles.prev} onClick={handlePrev}>
+          Назад
+        </button>
+        <button className={styles.next} onClick={handleNext}>
+          Вперёд
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
